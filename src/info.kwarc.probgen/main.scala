8a19824aec9659e8881dbeef0e7b12ce4545fc66
shaikhilhaam <shaikhilhaam14@gmail.com>
2026-01-28 11:38:59 +0100

    added MDP (VI/PI)

--- src/info.kwarc.probgen/main.scala
+++ src/info.kwarc.probgen/main.scala
@@ -3,7 +3,8 @@ package info.kwarc.probgen
 /** simple main method to call generators and see their results */
 object Test {
   def main(args: Array[String]): Unit = {
-    val p = SearchProblemGenerator.make()
+    /*val p = SearchProblemGenerator.make()*/
+    val p = MDPGenerator.make()
     val subs = p.chooseSubproblems()
     val stex = p.toSTeX(subs)
     println(stex)
