4b7210d4f3192208734ebb92747c720b5b5e12bc
mostafacontrols <mostafa.controls@gmail.com>
2026-04-15 11:24:40 +0200

    tOHTML added

--- src/info.kwarc.probgen/webserver.scala
+++ src/info.kwarc.probgen/webserver.scala
@@ -1,80 +1,75 @@
 package info.kwarc.probgen
 
+// Generators
 import info.kwarc.probgen.MDPGenerator
+import info.kwarc.probgen.BasicProbabilityProblemGenerator
+import info.kwarc.probgen.SearchProblemGenerator
+
 import com.sun.net.httpserver.HttpServer
 import java.net.InetSocketAddress
 import java.io.OutputStream
 
 object WebServer {
 
-  // Extract points if needed (safe)
-  def extractPoints(s: String): String = {
-    val r = "\\[pts=\\{(\\d+)\\}\\]".r
-    r.findFirstMatchIn(s).map(_.group(1)).getOrElse("")
-  }
-
-  // FINAL SAFE CLEANER (MathJax friendly)
-  def clean(s: String): String = {
-    s
-      // remove environments
-      .replaceAll("\\\\begin\\{.*?\\}", "")
-      .replaceAll("\\\\end\\{.*?\\}", "")
-
-      // remove grading / UI noise
-      .replaceAll("\\[pts=\\{[^}]*\\}\\]", "")
-      .replaceAll("\\[testspace=\\{[^}]*\\}\\]", "")
-      .replaceAll("%%%%%%%%+", "")
-      .replaceAll("Unknown environment 'itemize'", "")
-      .replaceAll("\\[\\]", "")
-
-      // FIX CORE STeX MACROS → LaTeX SAFE
-      .replaceAll("\\\\tup\\{(.*?)\\}", "($1)")
-      .replaceAll("\\\\mathtt\\{(.*?)\\}", "\\\\mathtt{$1}")
-      .replaceAll("\\\\set\\{(.*?)\\}", "\\\\{ $1 \\\\}")
-
-      // item lists
-      .replaceAll("\\\\item", "•")
-
-      // normalize spacing only
-      .replaceAll("\n{2,}", "<br><br>")
-      .replaceAll("\n", "<br>")
-  }
-
-  def generateProblem(): String = {
+  
+  // GENERATE 3 PROBLEMS 
+  
+  def generateProblems(): String = {
 
     val sb = new StringBuilder
-    val numProblems = 5
 
-    for (i <- 1 to numProblems) {
+    // ----------- MDP -----------
+    val mdp = MDPGenerator.make()
+    val mdpHtml = mdp.toSTeX(mdp.chooseSubproblems()).toHTML
 
-      val p = MDPGenerator.make()
-      val subs = p.chooseSubproblems()
+    sb.append(
+      s"""
+      <div class="problem">
+        <h2>MDP Problem</h2>
+        <div class="content">$mdpHtml</div>
+      </div>
+      """
+    )
 
-      val raw = p.toSTeX(subs).toString
-      val cleaned = clean(raw)
+    // ----------- PROBABILITY -----------
+    val prob = BasicProbabilityProblemGenerator.make()
+    val probHtml = prob.toSTeX(prob.chooseSubproblems()).toHTML
 
-      sb.append(
-        s"""
-        <div class="problem">
-          <h2>Problem $i</h2>
-          <div class="content">
-            $cleaned
-          </div>
-        </div>
-        """
-      )
-    }
+    sb.append(
+      s"""
+      <div class="problem">
+        <h2>Probability Problem</h2>
+        <div class="content">$probHtml</div>
+      </div>
+      """
+    )
+
+    // ----------- SEARCH -----------
+    val search = SearchProblemGenerator.make()
+    val searchHtml = search.toSTeX(search.chooseSubproblems()).toHTML
+
+    sb.append(
+      s"""
+      <div class="problem">
+        <h2>Search Problem</h2>
+        <div class="content">$searchHtml</div>
+      </div>
+      """
+    )
 
     sb.toString()
   }
 
+  
+  // MAIN SERVER
+  
   def main(args: Array[String]): Unit = {
 
     val server = HttpServer.create(new InetSocketAddress(8080), 0)
 
     server.createContext("/", exchange => {
 
-      val content = generateProblem()
+      val content = generateProblems()
 
       val html =
         s"""
@@ -83,10 +78,10 @@ object WebServer {
 <meta charset="UTF-8">
 <title>ProbGen</title>
 
+<!-- MathJax for LaTeX rendering -->
 <script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js"></script>
 
 <style>
-
 body {
   font-family: Arial;
   margin: 40px;
@@ -124,7 +119,6 @@ button {
 button:hover {
   background: #1c60b3;
 }
-
 </style>
 
 </head>
