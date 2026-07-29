55fd744e8e39be773c9fbe1d5b1c71330e83ee38
Sungonogi <sungonogi@gmail.com>
2026-07-29 09:55:14 +0200

    🚨 add warning

--- src/info.kwarc.probgen/Probabilities/generator.scala
+++ src/info.kwarc.probgen/Probabilities/generator.scala
@@ -4,6 +4,8 @@ package info.kwarc.probgen
 object BasicProbabilityProblemGenerator extends ProblemGenerator[BasicProbabilityProblem] {
 
   // only between 6 and 12 combinations
+  // WARNING: adding List(3,3,3) leads to a infinite loop because we enforce that for the conditional
+  // probability the condition A ? B  is true at most 6 times, but the minimum possible is 9
   private val domainSizeCombinations = List(
     List(2, 3),
     List(3, 2),
